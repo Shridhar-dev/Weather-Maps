d40a37f4b357f9f17e6772fbf7822449d032dc81
Shridhar-dev <shridharkamat10@gmail.com>
2020-10-01 10:56:26 +0530

    first one

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,11 @@ function Header() {
         <>
         <div class="container-fluid" style={{background:'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(130,221,245,1) 37%, rgba(39,190,247,1) 100%)'}}>
             <div class="row py-5">
-                <div class="col-12 col-sm-12 col-md-6 col-xl-6 ">
-                    <h1 class="display-2 text-center mx-5">Know the weather around the<strong> world</strong>🌍</h1>
+                <div class="col-12 col-sm-12 col-md-6 col-xl-6 py-2">
+                    <h1 class="display-2 text-center">Know the weather around the<strong> world</strong>🌍</h1>
                 </div>
-                <div class="col-12 col-sm-12 col-md-6 col-xl-6">
-                    <img src={wi} class="img-fluid mx-5" width="350"/>
+                <div class="col-12 col-sm-12 col-md-6 col-xl-6 py-2">
+                    <img src={wi} class="img-fluid" width="300rem"/>
                 </div>
             </div>
             
